d0d192deef9ea8d1940c7ada967572a44675d0f9
Allan Krama Guimarães <allan.guimaraes@wises.com.br>
2023-10-18 16:29:06 -0300

    Initial commit. Files from start.spring.io .

---
+++ src/main/java/kg/allan/purchasetransactions/PurchaseTransactionsApplication.java
@@ -0,0 +1,13 @@
+package kg.allan.purchasetransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/allan/purchasetransactions/PurchaseTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.allan.purchasetransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
